acd74fd5c75a77ab60e635863d7a320238be06f8
Marco <ingmarcodominguez@gmail.com>
2026-06-10 12:04:37 -0300

    one signal

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import OneSignal
+from 'react-onesignal'
+
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+OneSignal.init({
+
+  appId:
+    '193f058c-e58c-460c-a1ba-59524983e7ac',
+
+  notifyButton: {
+    enable: true
+  },
+
+  allowLocalhostAsSecureOrigin: true
+
+})
+
+createRoot(
+  document.getElementById('root')
+).render(
+
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+
